18fa52dbc33e9cbdc9967f631bc5ae09221d9ce5
mohamedIBRAHIEM <mohamed.ibrahim.lotfy.02468@gmail.com>
2024-02-07 11:13:47 +0200

    test

---
+++ src/main/java/org/example/HelloServlet.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.ws.rs.core.MediaType;
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType(MediaType.TEXT_HTML);
+        String[] name = request.getParameterValues("name");
+        response.getWriter().println("hello ya <b>" + String.join(" ", name) + "</b>");
+        response.getWriter().println("<img src='book.jpg'/>");
+        response.getWriter().println("<hr />");
+        response.getWriter().println("copyright (c)");
+        response.setStatus(201);
+        response.setHeader("my_test_hedears","test");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy method");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init method");
+    }
+}
